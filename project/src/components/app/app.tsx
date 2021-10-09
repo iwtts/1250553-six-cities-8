756d8e56a9c991621bf751e01808681e76d6dc80
iwtts <berzinao@hotmail.com>
2021-10-09 13:22:59 +0300

    3.1.2 Добавит маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,40 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 import Main from '../main/main';
+import Favourites from '../favorites/favorites';
+import Login from '../login/login';
+import NotFound from '../not-found/not-found';
+import Property from '../property/property';
 
 type AppProps = {
   cardsAmount: number;
 }
 
 function App({cardsAmount}: AppProps): JSX.Element {
-  return <Main cardsAmount = {cardsAmount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <Main
+            cardsAmount={cardsAmount}
+          />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <Login />
+        </Route>
+        <Route exact path={AppRoute.Favorites}>
+          <Favourites />
+        </Route>
+        <Route exact path={AppRoute.Room}>
+          <Property />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
