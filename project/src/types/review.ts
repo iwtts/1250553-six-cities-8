772fc753b64d5c367454c936ea35a26030293d09
iwtts <berzinao@hotmail.com>
2021-10-23 22:20:41 +0300

    4.1.10 Добавит моки отзывов

---
+++ project/src/types/review.ts
@@ -0,0 +1,14 @@
+type Review = {
+  comment: string,
+  date: string,
+  id: number,
+  rating: number,
+  user: {
+    avatarUrl: string,
+    id: number,
+    isPro: boolean,
+    name: string,
+  },
+}
+
+export type { Review };
