26b7ef2400328d4fae6a098796d5aee4e819d960
iwtts <berzinao@hotmail.com>
2021-10-23 15:38:41 +0300

    4.1.5 Обновит компонент «Главная страница»

--- project/src/components/main/main.tsx
+++ project/src/components/main/main.tsx
@@ -1,10 +1,13 @@
-import Card from '../card/card';
+import CardsList from '../cards-list/cards-list';
+
+import { Offer } from '../../types/offer';
 
 type MainProps = {
   cardsAmount: number;
+  offers: Offer[];
 }
 
-function Main({cardsAmount}: MainProps): JSX.Element {
+function Main({cardsAmount, offers}: MainProps): JSX.Element {
   return (
     <div className="page page--gray page--main">
       <header className="header">
@@ -93,9 +96,7 @@ function Main({cardsAmount}: MainProps): JSX.Element {
                   <li className="places__option" tabIndex={0}>Top rated first</li>
                 </ul>
               </form>
-              <div className="cities__places-list places__list tabs__content">
-                {new Array(cardsAmount).fill(undefined).map((card, i) => <Card key={card + i.toString()}/>)}
-              </div>
+              <CardsList offers={offers}/>
             </section>
             <div className="cities__right-section">
               <section className="cities__map map"></section>
