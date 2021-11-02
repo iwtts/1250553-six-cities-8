69e8313ab09689de9343686585737d5b79ccc074
iwtts <berzinao@hotmail.com>
2021-11-02 15:29:04 +0300

    6.1.2 Подключит redux

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -5,16 +5,28 @@ import App from './components/app/app';
 import { offers } from './mocks/offers';
 import { reviews } from './mocks/reviews';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { reducer } from './store/reducer';
+
 const Setting ={
   CARDS_AMOUNT: 5,
 } as const;
 
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      cardsAmount = {Setting.CARDS_AMOUNT}
-      offers = {offers}
-      reviews = {reviews}
-    />
+    <Provider store={store}>
+      <App
+        cardsAmount = {Setting.CARDS_AMOUNT}
+        offers = {offers}
+        reviews = {reviews}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
