4638d5a3935b9e5f1bc3cd69c45d1b16d94043b9
iwtts <berzinao@hotmail.com>
2021-11-05 12:12:08 +0300

    7.1.3 Добавит модуль с конфигурацией axios

---
+++ project/src/services/api.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosError, AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios';
+
+import { getToken } from './token';
+
+const BACKEND_URL = 'https://8.react.pages.academy/six-cities';
+const REQUEST_TIMEOUT = 5000;
+
+enum HttpCode {
+  Unauthorized = 401,
+}
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+
+    (error: AxiosError) => {
+      const {response} = error;
+
+      if (response?.status === HttpCode.Unauthorized) {
+        return false;
+      }
+
+      return Promise.reject(error);
+    },
+  );
+
+  api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
+
+  return api;
+};
